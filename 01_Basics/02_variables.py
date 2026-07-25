03e78016053002b01229bf597234856b2cb87ffe
Sai Keerthi Adepu <saikeerthiadepu@gmail.com>
2026-07-25 11:46:33 +0530

    Add variables demonstration program in Python
    This program demonstrates variable creation and printing in Python.

---
+++ 01_Basics/02_variables.py
@@ -0,0 +1,13 @@
+# Program: Variables in Python
+# Author: Saikeerthi Adepu
+# Description: This program demonstrates how to create and print variables.
+
+name = "Saikeerthi"
+age = 22
+cgpa = 7.32
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("CGPA:", cgpa)
+print("Student:", is_student)
