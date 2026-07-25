4b00336cdfca3cc41ec85488d4b5e688cb81619b
Sai Keerthi Adepu <saikeerthiadepu@gmail.com>
2026-07-25 11:47:50 +0530

    Add input and output demonstration program
    This program demonstrates how to take user input and display it.

---
+++ 01_Basics/03_input_output.py
@@ -0,0 +1,10 @@
+# Program: Input and Output in Python
+# Author: Saikeerthi Adepu
+# Description: This program demonstrates how to take user input and display it.
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("\n----- User Details -----")
+print("Name:", name)
+print("Age:", age)
